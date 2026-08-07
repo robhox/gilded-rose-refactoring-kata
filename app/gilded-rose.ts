a2bb761d03c278c5e4a3046022070ee904f1de11
Robin Hormaux <robinhormaux@gmail.com>
2026-08-07 16:23:05 +0200

    refactor: set expired backstage pass quality to zero

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -65,7 +65,7 @@ export class GildedRose {
             item.quality = item.quality - 1;
           }
         } else {
-          item.quality = item.quality - item.quality;
+          item.quality = 0;
         }
       } else {
         if (item.quality < 50) {
